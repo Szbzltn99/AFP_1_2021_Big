c386f023670cdc03d4eb8b34b9c0f2711839eca8
bldzsrbalazs <balazsboldizsar2000@gmail.com>
2021-11-16 09:15:07 +0100

    htdocs: routing.php added new routing pages

--- htdocs/protected/routing.php
+++ htdocs/protected/routing.php
@@ -14,9 +14,14 @@ $_GET['P'] = 'home';
 			require_once PROTECTED_DIR.'home.php';
 			break;
 		case 'logout': userLogout(); break;
-		
+		case 'answerSurvey' : 
+			isUserLoggedIn() ? require_once PROTECTED_DIR.'survey/ans_survey.php' : header('Location: index.php'); 
+			break;
 		case 'editUser' : 
 			isUserLoggedIn() ? require_once PROTECTED_DIR.'user/editUser.php' : header('Location: index.php');
+			break;
+		case 'newSurveyUpload':
+			isUserLoggedIn() && $_SESSION['permission'] == 1 ? require_once PROTECTED_DIR.'admin/add_survey.php' : header('Location: index.php');
 			break;
 		default: require_once PROTECTED_DIR.'404.php'; break;
 	}
