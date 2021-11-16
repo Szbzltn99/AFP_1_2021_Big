290f1d0b74c7187bced989ea6c2f983a90dadc94
ricsirobi <acs.robert2000@gmail.com>
2021-11-16 11:19:34 +0100

    htdocs: fixed gif to work without internet

--- htdocs/protected/home.php
+++ htdocs/protected/home.php
@@ -1,2 +1,2 @@
 <h1 style = "text-align: center;">Üdvözöllek a kérdőív oldalon!</h1>
-<div id="gif" class="gif"> <img src="https://images.milledcdn.com/2021-06-03/J9hEdHhxBUuAV24g/YwIKnDe0OIV9.gif"/>  </div>
+<div id="gif" class="gif"> <img src="public/toHome.gif"/>  </div>
