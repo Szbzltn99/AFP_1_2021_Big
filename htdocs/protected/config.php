33bc994e4812a106a295d7c29df1a1a0436c2597
IAmYeetler <67020263+IAmYeetler@users.noreply.github.com>
2021-11-16 08:39:21 +0100

    htdocs: config.php uploaded

---
+++ htdocs/protected/config.php
@@ -0,0 +1,8 @@
+<?php 
+define('BASE_DIR', './');
+define('PUBLIC_DIR', BASE_DIR.'public/');
+define('PROTECTED_DIR', BASE_DIR.'protected/');
+
+define('DATABASE_CONTROLLER', PROTECTED_DIR.'database.php');
+
+?>
