efe3948a5bc56b0deb4e3627c0b3f7403a32a4ac
bldzsrbalazs <balazsboldizsar2000@gmail.com>
2021-11-16 10:43:38 +0100

    htdocs: index.php title typo fixed

--- htdocs/index.php
+++ htdocs/index.php
@@ -12,7 +12,7 @@ if(!array_key_exists('P', $_GET) || empty($_GET['P']))
 <html>
     <head>
         <meta charset="utf-8">
-	    <title>Órarend</title>
+	    <title>Kérdőív</title>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
