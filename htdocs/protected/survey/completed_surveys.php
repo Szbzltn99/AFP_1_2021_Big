f67eadb49c89605ab303cf80f95a6264f023da26
Vereb Barna <intelbarna97@gmail.com>
2021-11-18 17:36:20 +0100

    feat: added listing to completed_surveys.php

--- htdocs/protected/survey/completed_surveys.php
+++ htdocs/protected/survey/completed_surveys.php
@@ -1,3 +1,12 @@
+<?php
+
+    $query = "SELECT s.sname, t.name FROM survey s, topic t, user_survey u WHERE u.uid=" . $_SESSION["uid"] . "  AND u.sid=s.sid AND u.answer!=0 GROUP BY s.sname";
+    $result = classList($query);
+    $n=0;
+?>
+<?php if($result === NULL || empty($result)): ?>
+    <p>Nincs rekord</p>
+    <?php else: ?>
 <html>
     <head>
     <link rel="stylesheet" href="<?php echo PUBLIC_DIR."style.css";?>">
@@ -15,14 +24,20 @@
                 </tr>
             </thead>
             <tbody>
+            <?php foreach($result as $row): $n=$n+1?>
                 <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td><button>Válaszok törlése</button><button>Módosítás</button></td>
+                <td><?=$n?></td>
+                <td><?=$row['sname']?></td>
+                <td><?=$row['name']?></td>
+                <td>
+                    <button name="del">Válaszok törlése</button>
+                    <button>Módosítás</button>
+                </td>
                 </tr>
+            <?php endforeach;?>
             </tbody>
             </table>
         </div>
     </body>
 </html>
+<?php endif; ?>
