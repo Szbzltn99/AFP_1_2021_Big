630ddeb94b6bafa033f9416c4c4267fbfdf5830d
bldzsrbalazs <balazsboldizsar2000@gmail.com>
2021-11-20 15:51:15 +0100

    htdocs: index.php added new css and added new styles

--- htdocs/index.php
+++ htdocs/index.php
@@ -20,7 +20,7 @@ if(!array_key_exists('P', $_GET) || empty($_GET['P']))
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        
+        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
         <link rel="stylesheet" href="<?php echo PUBLIC_DIR."style.css";?>">
         <script>
             function alertText(id,msg,alertType)
