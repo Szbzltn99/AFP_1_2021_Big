f673736b7556b7d21f91836292a79dc500f87996
Konya Donat <konya.donat@gmail.com>
2021-11-21 18:17:16 +0100

    htdocs: alert text script added to add_questin.php

--- htdocs/protected/admin/add_question.php
+++ htdocs/protected/admin/add_question.php
@@ -1,4 +1,17 @@
-<?php
-echo "<h1>Add question</h1>";
+        <script>
+            function alertText(id,msg,alertType)
+            {
+                if(alertType == "success")
+                {
+                document.getElementById(id).style.color = "#66CD00";
+                }
+                else
+                {
+                    document.getElementById(id).style.color = "crimson";
+                }
+                document.getElementById (id). innerHTML = msg;
+            }
+        </script>>
 
-?>
+    
+        
