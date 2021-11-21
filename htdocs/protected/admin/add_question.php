9bdcf3ce99b063e92d64a17cbaad8318407d88a3
Konya Donat <konya.donat@gmail.com>
2021-11-21 18:58:56 +0100

    htdocs: add_question answer one checkers added

--- htdocs/protected/admin/add_question.php
+++ htdocs/protected/admin/add_question.php
@@ -9,15 +9,15 @@
                 </div>
                 <div class="form-group">
                     <label for="answ1">Válasz érték 1 : </label>
-                    <input type="text" min=1 max=400 class="form-control" name="answ1" id="answ1" maxlength="255"/> <br/>
+                    <input type="text" class="form-control" name="answ1" id="answ1"/> <br/>
                 </div>
                 <div class="form-group">
                     <label for="answ2">Válasz érték 2 : </label>
-                    <input type="text" min=1 max=400 class="form-control" name="answ2" id="answ2" maxlength="255"/> <br/>
+                    <input type="text" class="form-control" name="answ2" id="answ2"/> <br/>
                 </div>
                 <div class="form-group">
                     <label for="answ3">Válasz érték 3 : </label>
-                    <input type="text" min=1 max=400 class="form-control" name="answ3" id="answ3" maxlength="255"/> <br/>
+                    <input type="text" class="form-control" name="answ3" id="answ3"/> <br/>
                 </div>
                 <button id="submit" type="submit" name="submit" class="btn btn-primary btn-lg btn-block">Mentés </button>
             </form>
@@ -45,6 +45,29 @@ if(isset($_POST["submit"]))
                 Swal.fire(
                     'Error!',
                     'The question you entered is too long! (more than 45 charaters)',
+                    'warning'
+                )
+            </script>
+        <?php
+    }
+    else if($_POST["answ1"] == null) {
+        ?>
+        <script>
+                Swal.fire(
+                    'Error!',
+                    'You did not enter any answer for option one!',
+                    'warning'
+                )
+            </script>
+        <?php
+        
+    }
+    else if($_POST["answ1"].is_string($_POST["answ1"]) == true) {
+        ?>
+        <script>
+                Swal.fire(
+                    'Error!',
+                    'The option value one is an integer!',
                     'warning'
                 )
             </script>
