10aa969f64e548ec8f2866b7dc0f33fe05070cc3
Konya Donat <konya.donat@gmail.com>
2021-11-21 18:26:47 +0100

    htdocs: add_question.php alert_text method replaced

--- htdocs/protected/admin/add_question.php
+++ htdocs/protected/admin/add_question.php
@@ -1,19 +1,3 @@
-        <script>
-            function alertText(id,msg,alertType)
-            {
-                if(alertType == "success")
-                {
-                document.getElementById(id).style.color = "#66CD00";
-                }
-                else
-                {
-                    document.getElementById(id).style.color = "crimson";
-                }
-                document.getElementById (id). innerHTML = msg;
-            }
-        </script>>
-
-    
 <br>
         <div class="container">
             <h2>Kérdés hozzáadása</h2>
