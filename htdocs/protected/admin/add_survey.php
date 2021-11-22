b80116f711de3b4d6136d843cc6d0fa9e19895b3
ricsirobi <acs.robert2000@gmail.com>
2021-11-22 21:45:51 +0100

    htdocs: updated add_survey.php
    Created form with inputs.
    Added scripts to backend.

--- htdocs/protected/admin/add_survey.php
+++ htdocs/protected/admin/add_survey.php
@@ -1,4 +1,140 @@
-<?php
-echo "<h1>Add survey</h1>";
+<html>
 
-?>
+<head>
+    <link rel="stylesheet" href="<?php echo PUBLIC_DIR . "style.css"; ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script>
+        function markQuestion(questionid) {
+            if (!document.getElementById("checkbox" + questionid).checked) {
+                document.getElementById("questions").value = document.getElementById("questions").value.replaceAll((questionid + ";"), "");
+                var orders = document.querySelectorAll(".order");
+                for (let i = 0; i <= document.querySelectorAll(".order").length - 1; i++) {
+                    console.log(i);
+                    if (orders[i].innerText > document.getElementById("order" + questionid).innerText && orders[i].innerText != "") {
+                        console.log("Megváltoztatom: " + i);
+                        orders[i].innerText = orders[i].innerText - 1;
+                    }
+                }
+                console.log("Leveszem:" + questionid);
+                document.getElementById("order" + questionid).innerText = "";
+            } else {
+                document.getElementById("questions").value += questionid + ";";
+                document.getElementById("order" + questionid).innerText = document.getElementById("questions").value.length / 2;
+                console.log("Hozzáadom: " + questionid);
+            }
+        }
+
+        function hideOrNot(divId) {
+            document.getElementById("theme").hidden = true;
+            document.getElementById("survey").hidden = true;
+            document.getElementById("questionsDiv").hidden = true;
+            document.getElementById(divId).hidden = false;
+        }
+    </script>
+</head>
+
+<body>
+    <?php
+    $listQuestionsResult = classList("sELECT * FROM `questions` WHERE 1");
+    ?>
+    <form method="post" action="">
+        <div class="usersDiv" class="container">
+            <h2>Mit szeretne tenni?</h2>
+            <table class="table">
+                <tr>
+                    <th scope="col">Téma hozzáadása</th>
+                    <th scope="col">Kérdőív létrehozása</th>
+                    <th scope="col">Kérdéseket kérdőívhez adni</th>
+                </tr>
+                <tr>
+                    <td> <input name='toDo' onchange='hideOrNot("theme")' type='radio' /> </td>
+                    <td> <input name='toDo' checked onchange='hideOrNot("survey")' type='radio' /> </td>
+                    <td> <input name='toDo' onchange='hideOrNot("questionsDiv")' type='radio' /> </td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="usersDiv" hidden id='theme' class="container">
+            <h2>Téma hozzáadása</h2>
+            <table class="table">
+                <tr>
+                    <th scope="col">Téma neve</th>
+                </tr>
+                <tr>
+                    <td> <input placeholder="Téma neve" type="text" /> </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type='submit' name="themeCreateButton" value='Téma Létrehozás'>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="usersDiv" id='survey' class="container">
+            <h2>Kérdőív létrehozása</h2>
+            <table class="table">
+                <tr>
+                    <th scope="col">Kérdőív neve</th>
+                    <th></th>
+                    <th scope="col">Kérdőív témája</th>
+                </tr>
+                <tr>
+                    <td> <input placeholder="Kérdőív neve" type="text" /> </td>
+                    <td> </td>
+                    <td> <input placeholder="Kérdőív témája" type="text" /> </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' name="surveyCreateButton" value='Kérdőív Létrehozás'>
+                    </td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+        <div class="usersDiv" hidden id='questionsDiv' class="container">
+            <?php if ($listQuestionsResult === NULL || empty($listQuestionsResult)) : ?>
+                <h2>Nincs egyetlen kérdés sem!</h2>
+            <?php else : ?>
+                <h2>Kérdések listája</h2>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Kérdés</th>
+                            <th scope="col">Válasz 1</th>
+                            <th scope="col">Válasz 2</th>
+                            <th scope="col">Válasz 3</th>
+                            <th scope="col">Hozzáadás</th>
+                            <th scope="col">Hozzáadási Sorrend</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($listQuestionsResult as $row) : ?>
+                            <tr scope="row" class="list">
+                                <td scope="row"><?= $row['qid'] ?></td>
+                                <td scope="row"><?= $row['question'] ?></td>
+                                <td scope="row"><?= $row['answer1'] ?></td>
+                                <td scope="row"><?= $row['answer2'] ?></td>
+                                <td scope="row"><?= $row['answer3'] ?></td>
+                                <td> <input type="checkbox" onclick="markQuestion('<?= $row['qid'] ?>')" id='checkbox<?= $row['qid'] ?>' value="<?= $row['qid'] ?>"></input> </td>
+                                <td class='order' id='<?= "order" . $row['qid'] ?>'> </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+                <table class="table" style="border:transparent;">
+                    <tr>
+                        <td>
+                            <input type="submit" name="questionsAddButton" value="Kérdések hozzáadása">
+                        </td>
+                    </tr>
+                </table>
+
+                <input id='questions' name='questionsToAdd' value="" hidden placeholder="Hidden lesz"></input>
+            <?php endif; ?>
+        </div>
+    </form>
+</body>
+
+</html>
