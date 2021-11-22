965c566193dd250a02a5d3315d9e67528156ef09
Konya Donat <konya.donat@gmail.com>
2021-11-22 17:43:16 +0100

    htdocs: average.php file added to routing.php

--- htdocs/protected/routing.php
+++ htdocs/protected/routing.php
@@ -16,6 +16,7 @@ $_GET['P'] = 'home';
 		case 'completedSurveys'  : isUserLoggedIn() ? require_once PROTECTED_DIR.'survey/completed_surveys.php' : header('Location: index.php'); break;
 		case 'topics'  : isUserLoggedIn() ? require_once PROTECTED_DIR.'survey/topics.php' : header('Location: index.php'); break;
 		case 'modifyUserAdmin' : isUserLoggedIn() && $_SESSION['permission'] > 0 ? require_once PROTECTED_DIR.'admin/modifyUserFromAdmin.php' : header('Location: index.php'); break;
+		case 'averages' : isUserLoggedIn() ? require_once PROTECTED_DIR.'survey/average.php' : header('Location: index.php'); break;
 		default: require_once PROTECTED_DIR.'404.php'; break;
 	}
 
