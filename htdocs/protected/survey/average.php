6a4472cd021d459244aa6dd32ca276e26f60aa9b
Konya Donat <konya.donat@gmail.com>
2021-11-22 17:50:39 +0100

    htdocs: average.php comments added,TODO marked

---
+++ htdocs/protected/survey/average.php
@@ -0,0 +1,12 @@
+<br> 
+<div class="container">
+    Átlag válaszok megjelenítése
+</div>
+
+
+<?php
+
+ //TODO
+    //SQL lekérdezés ami megnézi az átlagot minden egyes kérdőívre, illetve az azokhoz kapcsolódó kérdésekhez
+        //majd azokat megjeleníti az adott kérdőívhez kapcsolódóan, de lehet úgy lesz megoldva hogy rákattintunk egy adott kérdőívre,
+        //aztán a kérdőívhez tartozó kérdéseket felsorlja, majd a kérdések mellé írja ki a felhasználók által adott válaszok többségét
