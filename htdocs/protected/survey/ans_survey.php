cdd1c15286c4d7fa3a58d15bd1abd114a5e7e0d8
ricsirobi <acs.robert2000@gmail.com>
2021-11-23 03:17:55 +0100

    htdocs: updated ans_survey.php
    Created radio buttons for the answers.

--- htdocs/protected/survey/ans_survey.php
+++ htdocs/protected/survey/ans_survey.php
@@ -1,7 +1,58 @@
 <?php
-if (isset($_POST["selectedSurvey"])) 
-{
+
+if (isset($_POST["selectedSurvey"])) {
+    $surveyID =  $_POST["selectedSurvey"];
+    $sql = "select * from questions, survey_question, survey where survey_question.qid = questions.qid and survey_question.sid = $surveyID and survey.sid = survey_question.sid";
     
+    echo $sql;
+    $questions = classList($sql);
+    if ($questions === NULL || empty($questions)) {
+        echo "Nincs kérdés";
+    } else {
+        echo '<form method="post" action=""> <div class="usersDiv" id=\'surveyDiv\' class="container"> <table class="table">';
+        $surveyName = $questions[0]["sname"];
+        echo "<h1>$surveyName</h1>";
+        echo "<tr><th>
+        Kérdés:
+        </th>
+        <th> Válasz1</th>
+        <th> Válasz2</th>
+        <th> Válasz3</th>
+        </tr>
+        ";
+        $names = "";
+        foreach ($questions as $questionsRow) {
+            $oneQuestion = $questionsRow["question"];
+            $a1 = $questionsRow["answer1"];
+            $a2 = $questionsRow["answer2"];
+            $a3 = $questionsRow["answer3"];
+            $qid = $questionsRow["qid"];
+            echo "<tr>";
+            echo "<td>";
+            echo $oneQuestion;
+            echo "</td>";
+            echo "<td>";
+            echo "<input type = 'radio' name = '$qid' value = '$a1'> $a1 ";
+            echo "</td>";
+            echo "<td>";
+            echo "<input type = 'radio' name = '$qid' value = '$a2'> $a2";
+            echo "</td>";
+            echo "<td>";
+            echo "<input type = 'radio' name = '$qid' value = '$a3'> $a3 ";
+            echo "</td>";
+            echo "</tr>";
+            $names .= $qid.";";
+        }
+        echo "<input type = 'text' value = '$names' name = 'names' hidden>";
+        echo "
+        </th>
+        <th>  </th>
+        <th> <div> <input type = 'submit' name = 'submited' value = 'Válaszok beküldése'></div> </th>
+        <th>  </th>
+        </tr>
+        ";
+        echo "</table></div></form>";
+    }
 } else {
     if (isUserLoggedIn()) {
         $userId = isUserLoggedIn();
