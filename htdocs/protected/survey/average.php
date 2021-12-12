5e49ef17e7f6ded0d64c2d613b7986987a2ea6b2
Konya Donat <konya.donat@gmail.com>
2021-12-12 19:57:59 +0100

    htdocs: average.php basic frontend finished

--- htdocs/protected/survey/average.php
+++ htdocs/protected/survey/average.php
@@ -1,12 +1,42 @@
-<br> 
-<div class="container">
-    Átlag válaszok megjelenítése
-</div>
+<html>
+    <head>
+    <link rel="stylesheet" href="<?php echo PUBLIC_DIR."style.css";?>"><meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <?php 
+        $query = "SELECT * FROM survey";
 
+        $listSurveyResult = classList($query);
 
-<?php
-
- //TODO
-    //SQL lekérdezés ami megnézi az átlagot minden egyes kérdőívre, illetve az azokhoz kapcsolódó kérdésekhez
-        //majd azokat megjeleníti az adott kérdőívhez kapcsolódóan, de lehet úgy lesz megoldva hogy rákattintunk egy adott kérdőívre,
-        //aztán a kérdőívhez tartozó kérdéseket felsorlja, majd a kérdések mellé írja ki a felhasználók által adott válaszok többségét
+        ?>
+        <div class = "usersDiv" class = "container">
+            <?php  if($listSurveyResult === NULL || empty($listSurveyResult)): ?>
+                <h2>Nincs egyetlen kérdőív sem!</h2>
+            <?php else: ?>
+            <h2>Kérdőívek listája</h2>
+            <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Kérdőív neve</th>
+                <th scope="col">Kérdőív témája</th>
+                <th scope="col">Megtekintés</th>
+                </tr>
+            </thead>
+            <tbody>
+            <form method = "post">
+            <?php foreach($listSurveyResult as $row): ?>
+                <tr scope="row" class = "list">
+                            <td scope="row"><?=$row['sname']?></td>
+                            <td scope="row"><?=$row['topic']?></td>
+                            <td scope="row">
+                            <a href="<?=BASE_URL?>?P=averagesspecific&G=<?=$row['sid']?>">Megtekint</a>
+                            </td>
+                        </tr>
+            <?php endforeach;?>
+            </tbody>
+            </table>
+            <?php endif; ?>
+            </form>
+        </div>
+    </body>
+</html>
