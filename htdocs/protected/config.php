b44e3f4a44f5476972ef3ef9602d5d94e6e9883d
Konya Donat <konya.donat@gmail.com>
2021-12-12 19:54:44 +0100

    htdocs: base url added to config.php

--- htdocs/protected/config.php
+++ htdocs/protected/config.php
@@ -5,4 +5,5 @@ define('PROTECTED_DIR', BASE_DIR.'protected/');
 
 define('DATABASE_CONTROLLER', PROTECTED_DIR.'database.php');
 
+define('BASE_URL','http://localhost/AFP_1_2021_BIG/htdocs/index.php');
 ?>
