01c1d8a8c912e7a126b7a6520acfbe2dedeef587
Vereb Barna <intelbarna97@gmail.com>
2022-03-18 00:30:06 +0100

    feat: can complete questions in uncompleted_surveys.php

--- htdocs/protected/survey/uncompleted_surveys.php
+++ htdocs/protected/survey/uncompleted_surveys.php
@@ -1,4 +1,5 @@
 <?php
+
     if($_SESSION!=NULL)
     {
         $query = "SELECT * FROM user_survey u WHERE u.uid=" . $_SESSION["uid"];
@@ -8,41 +9,79 @@
     {
         $query = "SELECT * FROM survey, topic WHERE survey.topic=topic.tid";
         $result=classList($query);
-        $newsurveys=NULL;
+        $newsurveys = [];
+    foreach($result as $temp)
+    {
+        array_push($newsurveys,$temp["sid"]);
+    }
+    //var_dump($newsurveys);
     }
     else
     {
 
-    $query = "SELECT surv.sname, top.name, surv.sid FROM survey surv, topic top, 
-    (SELECT s.sname, t.name, s.sid FROM survey s, topic t, 
-    (SELECT t.sid, t.uid FROM 
-    (
-    SELECT us.sid,us.uid,COUNT(us.questionid) q_db FROM 
-    user_survey us 
-    GROUP BY us.sid,us.uid
-    ) t 
-    WHERE q_db = 
-    (
-    SELECT COUNT(sq.qid) FROM 
-    survey_question sq 
-    WHERE sq.sid = t.sid)
-    ) mok 
-    WHERE mok.uid=" . $_SESSION["uid"] . "
-    AND s.sid=mok.sid 
-    AND s.topic=t.tid) giga 
-    WHERE surv.sid!=giga.sid 
-    AND surv.topic=top.tid;";
+    $query = "SELECT
+                    surv.sname,
+                    top.name,
+                    surv.sid
+                FROM
+                    survey surv,
+                    topic top
+                WHERE NOT
+                    EXISTS(
+                    SELECT
+                        s.sname,
+                        t.name,
+                        s.sid
+                    FROM
+                        survey s,
+                        topic t,
+                        (
+                        SELECT
+                            t.sid,
+                            t.uid
+                        FROM
+                            (
+                            SELECT
+                                us.sid,
+                                us.uid,
+                                COUNT(us.questionid) q_db
+                            FROM
+                                user_survey us
+                            GROUP BY
+                                us.sid,
+                                us.uid
+                        ) t
+                    WHERE
+                        q_db =(
+                        SELECT
+                            COUNT(sq.qid)
+                        FROM
+                            survey_question sq
+                        WHERE
+                            sq.sid = t.sid
+                    )
+                    ) mok
+                WHERE
+                    mok.uid = " . $_SESSION["uid"] . " AND s.sid = mok.sid AND s.topic = t.tid AND s.sid = surv.sid
+                ) AND surv.topic = top.tid;";
 
     $result = classList($query);
 
     $query="SELECT s.sid FROM survey s
     WHERE s.sid NOT IN 
     (SELECT u.sid FROM user_survey u
-    WHERE u.sid = " . $_SESSION["uid"] . "
+    WHERE u.uid = " . $_SESSION["uid"] . "
     )";
     
 
-    $newsurveys = classList($query);
+    $surveys = classList($query);
+    $newsurveys = [];
+    foreach($surveys as $temp)
+    {
+        array_push($newsurveys,$temp["sid"]);
+    }
+    //var_dump($newsurveys);
+    //var_dump($result);
     }
     }
 /*
@@ -53,19 +92,97 @@
         $result = classList($query);
 */
 
-    if (isset($_POST["fill"]))
+    if (isset($_POST["names"]))
    {
-
-       header("Refresh:0");      //ide jön majd a link a kitöltőhöz
+    $names = explode(";", $_POST["names"]);
+    
+    foreach ($names as $name) {
+        $num = "qid_" . $name;
+        $ans =  @$_POST["$num"];
+        if ($ans != "") {
+            if ($name != "") {
+                if (isUserLoggedIn()) {
+                    $userId = $_SESSION["uid"];
+                    $uploadans = "";
+                } else {
+                    $userId = -1;
+                    $uploadans = "";
+                }
+                $sid = $_POST["sid"];
+                $sql = "INSERT INTO user_survey(sid, uid, answer, questionid) VALUES ($sid,$userId,$ans,$name)";
+                executeQuery($sql);
+            }
+        }
+    }
+    echo "<br>Válaszaidat sikeresen rögzítettük.<br>";
    }
 
-   if (isset($_POST["re"]))
-   {
-       
-       header("Refresh:0");   //ide jön a link a folytatáshoz a kitöltőnél
-   }
-?>
-<?php if($result === NULL || empty($result)): ?>
+   if (isset($_POST["selected"])): 
+   
+    $surveyID=$_POST["selected"];
+    $query = "SELECT
+    *
+FROM
+    questions,
+    survey_question,
+    survey
+WHERE
+    questions.qid NOT IN(SELECT
+        questionid
+    FROM
+        user_survey
+    WHERE
+        user_survey.sid = $surveyID
+) AND questions.qid=survey_question.qid AND survey_question.sid=$surveyID AND survey.sid=survey_question.sid;";
+    $questions=classList($query);
+    //var_dump($questions);
+   ?>
+   <div class = "usersDiv">
+            <h2><?=$questions[0]["sname"]?></h2>
+            <table class="table">
+            <form method="post">
+            <thead>
+                <tr>
+                <th>Kérdés</th>
+                <th>1. Válasz</th>
+                <th>2. Válasz</th>
+                <th>3. Válasz</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php 
+            $names = "";
+            foreach($questions as $row): ?>
+                <tr>
+                <td><?=$row['question']?></td>
+                <td>
+                    <input name="qid_<?=$row['qid']?>" value =<?= $row['answer1']?> type='radio'><?=$row['answer1']?></td>
+                <td>
+                    <input name="qid_<?=$row['qid']?>" value =<?= $row['answer2']?> type='radio'><?=$row['answer2']?></td>                
+                <td>
+                    <input name="qid_<?=$row['qid']?>" value =<?= $row['answer3']?> type='radio'><?=$row['answer3']?></td>
+                </tr>
+            <?php 
+            $names .= $row['qid'] . ";";
+        endforeach;
+        $names = rtrim($names,";");?>
+        <input type = 'text' value = '<?=$names?>' name = 'names' hidden>
+        <input type = 'text' hidden value = '<?=$row['sid']?>' name ='sid'>
+            </th>
+        <th>  </th>
+        <th> <div> <input type = 'submit' name = 'submited' value = 'Válaszok beküldése'></div> </th>
+        <th>  </th>
+        </tr>
+            </tbody>
+            </form>
+            </table>
+        </div>   
+                    <?php endif; ?>
+    <?php
+
+   ?>
+   <?php
+ if($result === NULL || empty($result)): ?>
     <p>Nincs rekord</p>
     <?php else: ?>
 <html>
@@ -82,27 +199,29 @@
                 <th>Azonosító</th>
                 <th>Kérdőív</th>
                 <th>Téma</th>
-                <?php if($_SESSION!=NULL && $newsurveys!=NULL): ?>
+                <?php if($_SESSION!=NULL): ?>
                 <th>Kitöltés állapota</th>
                 <?php endif; ?>
                 <th>Művelet</th>
                 </tr>
             </thead>
             <tbody>
-            <?php foreach($result as $row): ?>
+            <?php 
+            foreach($result as $row): ?>
                 <tr>
                 <td><?=$row['sid']?></td>
                 <td><?=$row['sname']?></td>
                 <td><?=$row['name']?></td>
-                <?php if($newsurveys!=NULL && isset($newsurveys) && in_array($row['sid'],$newsurveys[0])):?>
+                <?php if($newsurveys!=NULL && isset($newsurveys) && in_array($row['sid'],$newsurveys)):?>
                 <td>Nincs elkezdve</td>
-                <?php elseif(isset($newsurveys)): ?>
+                <?php elseif(true): ?>
                 <td>El van kezdve</td>
                 <?php endif; ?>
                 <td>
-                    <button name="fill" value =<?= $row['sid']?>>Kitöltés</button>
-                    <?php if($newsurveys!=NULL && $_SESSION!=NULL && !in_array($row['sid'],$newsurveys[0])): ?>
-                    <button name="re" value =<?= $row['sid']?>>Folyatás</button>
+                    <?php if($newsurveys!=NULL && $_SESSION!=NULL && !in_array($row['sid'],$newsurveys)): ?>
+                    <button name="selected" value =<?= $row['sid']?>>Folyatás</button>
+                    <?php elseif(true): ?>                        
+                    <button name="selected" value =<?= $row['sid']?>>Kitöltés</button>
                     <?php endif; ?>
                 </td>
                 </tr>
